5917c810fea859439d86c03f1133c44c68a0ad66
nev <cy823236858@gmail.com>
2023-09-17 16:28:03 +0800

    update zero123

--- zero123/merge_paramters.py
+++ zero123/merge_paramters.py
@@ -11,8 +11,8 @@ zero_123 = torch.load(path1)
 control =torch.load(path2)
 # print(type(control))
 # print('cond_stage_model.transformer.text_model.embeddings.position_ids' in control)
-zero_state= zero_123
-control_state = control["state_dict"]
+zero_state= zero_123["state_dict"]
+control_state = control
 
 
 has_keys = 0
