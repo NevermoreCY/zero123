ae6fb724bd72cb22f84ada0d1b3d612033aabec5
nev <cy823236858@gmail.com>
2023-09-18 22:09:54 +0800

    update zero123

--- zero123/main.py
+++ zero123/main.py
@@ -846,7 +846,7 @@ if __name__ == "__main__":
         callbacks_cfg["checkpoint_callback"]["params"]['save_top_k'] = 10
         # callbacks_cfg["checkpoint_callback"]["params"]['save_last'] = None
         callbacks_cfg["checkpoint_callback"]["params"]['filename'] = '{epoch}-{step}'
-        callbacks_cfg["checkpoint_callback"]["params"]['mode'] = 'min'
+        #callbacks_cfg["checkpoint_callback"]["params"]['mode'] = 'min'
         callbacks_cfg["checkpoint_callback"]["params"]['monitor'] ='global_step'
         del callbacks_cfg["checkpoint_callback"]["params"]['save_top_k']
         # del callbacks_cfg["checkpoint_callback"]["params"]['save_last']
