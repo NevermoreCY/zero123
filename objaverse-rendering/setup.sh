2905417df98a2f5278baeaa13b0a86c16ae97020
nev <cy823236858@gmail.com>
2023-10-28 00:34:45 +0800

    developing

--- objaverse-rendering/setup.sh
+++ objaverse-rendering/setup.sh
@@ -1,7 +1,7 @@
 # install blender 3.2.2
-wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
-tar -xf blender-3.2.2-linux-x64.tar.xz
-rm blender-3.2.2-linux-x64.tar.xz
+#wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
+#tar -xf blender-3.2.2-linux-x64.tar.xz
+#rm blender-3.2.2-linux-x64.tar.xz
 
 # this is needed to download urls in blender
 # https://github.com/python-poetry/poetry/issues/5117#issuecomment-1058747106
