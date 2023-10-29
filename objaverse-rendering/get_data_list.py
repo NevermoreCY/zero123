0062434fabd7395e9f85db6855d856c74cce252b
nev <cy823236858@gmail.com>
2023-10-30 03:05:32 +0800

    developing

--- objaverse-rendering/get_data_list.py
+++ objaverse-rendering/get_data_list.py
@@ -4,6 +4,6 @@ import os
 view_dir = 'views'
 data_list = os.listdir(view_dir)
 out_path = 'shapenet_v1.json'
-print("num of samples: " , data_list)
+print("num of samples: " , len(data_list))
 with open(out_path, 'w') as f:
     json.dump(data_list,f)
