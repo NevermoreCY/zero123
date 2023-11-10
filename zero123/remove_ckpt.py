9ef9526e8f29550d5c8672e5dcc7a28b49a04aa7
nev <cy823236858@gmail.com>
2023-11-10 23:21:23 +0800

    developing

--- zero123/remove_ckpt.py
+++ zero123/remove_ckpt.py
@@ -1,6 +1,6 @@
 import os
 
-prefix = 'logs/2023-11-07T18-23-24_zero123_shape_turbo/checkpoints'
+prefix = 'logs/2023-11-07T18-23-24_zero123_shape_turbo/checkpoints/'
 
 ckpts = os.listdir(prefix)
 
@@ -8,3 +8,5 @@ for ckpt in ckpts:
     if ckpt not in ['last.ckpt', 'early']:
         epoch_n = (ckpt.split('-')[0]).split('=')[1]
         print(epoch_n)
+        if int(epoch_n) <= 25:
+            os.system('mv '+ prefix + ckpt + " " + prefix + 'early/'+ckpt )
